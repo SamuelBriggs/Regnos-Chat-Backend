2f3c1f6fb95dff407de39422562e3e7f73151b38
Alawode Samuel <tolulope.alawode@gmail.com>
2023-06-12 17:10:01 +0100

    added Login functionality to user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,13 @@ const bcrypt = require("bcrypt");
 const router = require("express").Router()
 
 router.post("/register", async (req, res) =>{
-
+        const foundUser = await User.findOne({userName: req.body.userName})
+        if (foundUser){
+          return res.send({
+                success: false,
+                message: "User already exists, try another username"
+            })
+        }
         const hashPassword = await bcrypt.hash(req.body.password, 10);
         req.body.password = hashPassword;
         const newUser = new User(req.body);
@@ -15,4 +21,24 @@ router.post("/register", async (req, res) =>{
 
 } )
 
+router.post("/login", async (req, res) => {
+        const user = await User.findOne({userName: req.body.userName})
+        if(!user){
+            return res.send({
+                success:false,
+                message: "User with this username not found. Please try again"
+            })
+        }
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        if(!validPassword){
+           return res.send({
+                success:false,
+                message:"password is not right"
+            })
+        }
+        res.send({
+            success: true,
+            message: "Login successful, Welcome"
+        })
+})
 module.exports = router;
